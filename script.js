375f98cffa1cf42385b314cf870fa3b56bb5fc53
Myrzagul <mayaibragimova04@gmail.com>
2024-08-06 22:56:46 +0300

    не закончен

---
+++ script.js
@@ -0,0 +1,20 @@
+const posts = {
+  name: 'posts',
+  isLoding: true,
+  byId: {
+    post1: {
+      id:'post1',
+      author:'user1',
+      body:'......',
+      comments: ['comment1', 'comment2'],
+    },
+    post2: {
+      id: 'post2',
+      auther: 'user2',
+      body:'......',
+      comment:['comment3', 'comment4', 'comment5'],
+    },
+  },
+  allIds: ['post1', 'post2'],
+}
+ const posts1 = []
